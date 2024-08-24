1430512b7425e83ba3df3973a3aba9f3b652edf7
treyrichbourg <treyrichbourg@gmail.com>
2024-08-23 22:15:01 -0600

    setup lsp attach

--- nvim/lua/comet/init.lua
+++ nvim/lua/comet/init.lua
@@ -2,19 +2,68 @@ require("comet.remap")
 require("comet.set")
 require("comet.lazy")
 
---Removes all trailing whitespace on save
---autocmd({"BufWritePre"}, {
---    group = comet,
---    pattern = "*",
---    command = [[%s/\s\+$//e]],
---})
+local augroup = vim.api.nvim_create_augroup
+local comet = augroup("comet", { clear = true })
+local autocmd = vim.api.nvim_create_autocmd
 
---Highlight when yanking text
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+--Removes all trailing whitespace on save
+autocmd({ "BufWritePre" }, {
+	group = comet,
+	pattern = "*",
+	command = [[%s/\s\+$//e]],
 })
 
+--Highlight when yanking text
+autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
+autocmd("LspAttach", {
+	group = comet,
+	callback = function(e)
+		local opts = { buffer = e.buf }
+		vim.keymap.set("n", "gD", function()
+			vim.lsp.buf.declaration()
+		end, opts)
+		vim.keymap.set("n", "gd", function()
+			vim.lsp.buf.definition()
+		end, opts)
+		vim.keymap.set("n", "K", function()
+			vim.lsp.buf.hover()
+		end, opts)
+		vim.keymap.set("n", "gi", function()
+			vim.lsp.buf.implementation()
+		end, opts)
+		vim.keymap.set("n", "<C-k>", function()
+			vim.lsp.buf.signature_help()
+		end, opts)
+		vim.keymap.set("n", "<space>wa", function()
+			vim.lsp.buf.add_workspace_folder()
+		end, opts)
+		vim.keymap.set("n", "<space>wr", function()
+			vim.lsp.buf.remove_workspace_folder()
+		end, opts)
+		vim.keymap.set("n", "<space>wl", function()
+			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+		end, opts)
+		vim.keymap.set("n", "<space>D", function()
+			vim.lsp.buf.type_definition()
+		end, opts)
+		vim.keymap.set("n", "<space>rn", function()
+			vim.lsp.buf.rename()
+		end, opts)
+		vim.keymap.set("n", "<space>ca", function()
+			vim.lsp.buf.code_action()
+		end, opts)
+		vim.keymap.set("n", "gr", function()
+			vim.lsp.buf.references()
+		end, opts)
+		vim.keymap.set("n", "<space>f", function()
+			vim.lsp.buf.format({ async = true })
+		end, opts)
+	end,
+})
--- nvim/lua/comet/plugins/colorscheme.lua
+++ nvim/lua/comet/plugins/colorscheme.lua
@@ -1,43 +1,42 @@
 --Colorschemes
 return {
-    {
-        'catppuccin/nvim',
-        name = 'catppuccin',
-        config = function()
-            mocha = "colorscheme catppuccin-mocha"
-            frappe = "colorscheme catppuccin-frappe"
-            macchiato = "colorscheme catppuccin-macchiato"
-            latte = "colorscheme catppuccin-latte"
-            vim.cmd(frappe) --Active
-        end
-    },
-    {
-        'rebelot/kanagawa.nvim',
-        name = 'kanagawa-wave',
-        config = function()
-            wave = "colorscheme kanagawa-wave"
-            dragon = "colorscheme kanagawa-dragon"
-            lotus = "colorscheme kanagawa-lotus"
-            --vim.cmd(wave) --Active
-        end
-    },
-    {
-        'rose-pine/neovim',
-        name = 'rose-pine',
-        config = function()
-            rose = "colorscheme rose-pine"
-        end
-    },
-    {
-        'folke/tokyonight.nvim',
-        name = 'tokyonight',
-        config = function()
-            night = "colorscheme tokyonight-night"
-            storm = "colorscheme tokyonight-storm"
-            moon = "colorscheme tokyonight-moon"
-            day = "colorscheme tokyonight-day"
-            --vim.cmd(night)
-        end
-    },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		config = function()
+			mocha = "colorscheme catppuccin-mocha"
+			frappe = "colorscheme catppuccin-frappe"
+			macchiato = "colorscheme catppuccin-macchiato"
+			latte = "colorscheme catppuccin-latte"
+			vim.cmd(frappe) --Active
+		end,
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		name = "kanagawa-wave",
+		config = function()
+			wave = "colorscheme kanagawa-wave"
+			dragon = "colorscheme kanagawa-dragon"
+			lotus = "colorscheme kanagawa-lotus"
+			--vim.cmd(wave) --Active
+		end,
+	},
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		config = function()
+			rose = "colorscheme rose-pine"
+		end,
+	},
+	{
+		"folke/tokyonight.nvim",
+		name = "tokyonight",
+		config = function()
+			night = "colorscheme tokyonight-night"
+			storm = "colorscheme tokyonight-storm"
+			moon = "colorscheme tokyonight-moon"
+			day = "colorscheme tokyonight-day"
+			--vim.cmd(night)
+		end,
+	},
 }
-
