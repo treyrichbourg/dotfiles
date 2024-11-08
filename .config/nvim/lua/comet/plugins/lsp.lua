1a7c3d24bcf6e446031e7ae81b986396f45112a3
treyrichbourg <treyrichbourg@gmail.com>
2024-11-07 23:54:25 -0700

    move lsp keymaps to lsp.lua

--- .config/nvim/lua/comet/plugins/lsp.lua
+++ .config/nvim/lua/comet/plugins/lsp.lua
@@ -13,6 +13,37 @@ return {
 	},
 	config = function()
 		require("mason").setup()
+
+		-- Capabilities
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+
+		-- LSP keymaps
+		local function on_attach(_, bufnr)
+			local opts = { buffer = bufnr }
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+			vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+			vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+			vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+			vim.keymap.set("n", "<space>wl", function()
+				print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+			end, opts)
+			vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+			vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+			vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, opts)
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+			vim.keymap.set("n", "<space>f", function()
+				if vim.lsp.buf.server_capabilities().documentFormattingProvider then
+					vim.lsp.buf.format({ async = true })
+				else
+					vim.notify("Formatting not supported by attached LSP.")
+				end
+			end, opts)
+		end
+
 		--Language servers
 		require("mason-lspconfig").setup({
 			ensure_installed = {
@@ -20,14 +51,21 @@ return {
 				"lua_ls",
 				"ts_ls",
 				"bashls",
+				"gopls",
 			},
 			handlers = {
 				--Default handler
 				function(server_name)
-					require("lspconfig")[server_name].setup({})
+					require("lspconfig")[server_name].setup({
+						capabilities = capabilities,
+						on_attach = on_attach,
+					})
 				end,
+				-- Lua specific
 				["lua_ls"] = function()
 					require("lspconfig")["lua_ls"].setup({
+						capabilities = capabilities,
+						on_attach = on_attach,
 						settings = {
 							Lua = {
 								diagnostics = {
@@ -37,11 +75,61 @@ return {
 						},
 					})
 				end,
+				-- Go specific
+				["gopls"] = function()
+					require("lspconfig").gopls.setup({
+						capabilities = capabilities,
+						on_attach = on_attach,
+						settings = {
+							gopls = {
+								analyses = {
+									unusedparams = true,
+									nilness = true,
+									unusedwrite = true,
+									shadow = true,
+								},
+								staticcheck = true,
+								gofumpt = true,
+							},
+						},
+					})
+				end,
+				-- Python specific
+				["pyright"] = function()
+					require("lspconfig").pyright.setup({
+						capabilities = capabilities,
+						on_attach = on_attach,
+						settings = {
+							python = {
+								analyses = {
+									typeCheckingMode = "basic",
+									autoSearchPaths = true,
+									useLibraryCodeForTypes = true,
+								},
+							},
+						},
+					})
+				end,
+				-- Typescript specific
+				["ts_ls"] = function()
+					require("lspconfig").ts_ls.setup({
+						capabilities = capabilities,
+						on_attach = on_attach,
+						settings = {
+							javascript = {
+								format = { enable = true },
+								lint = { enable = true, lintOnSave = true },
+							},
+							typescript = {
+								format = { enable = true },
+								lint = { enable = true, lintOnSave = true },
+							},
+						},
+					})
+				end,
 			},
 		})
-		-- Capabilities
-		local capabilities = vim.lsp.protocol.make_client_capabilities()
-		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+
 		--Autocomplete
 		local cmp = require("cmp")
 		local cmp_select = { behavior = cmp.SelectBehavior.Select }
@@ -61,6 +149,7 @@ return {
 				{ name = "buffer" },
 			}),
 		})
+
 		--Formatters and Linters
 		require("mason-tool-installer").setup({
 			ensure_installed = {
