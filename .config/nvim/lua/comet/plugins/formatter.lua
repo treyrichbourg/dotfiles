c3ec276ba587763ec7453d1b715999d87764402d
treyrichbourg <treyrichbourg@gmail.com>
2026-02-23 23:42:35 -0700

    simplify conform.nvim setup

--- .config/nvim/lua/comet/plugins/formatter.lua
+++ .config/nvim/lua/comet/plugins/formatter.lua
@@ -1,31 +1,29 @@
 return {
 	"stevearc/conform.nvim",
 	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local conform = require("conform")
-		conform.setup({
-			formatters_by_ft = {
-				javascript = { "prettier" },
-				typescript = { "prettier" },
-				javascriptreact = { "prettier" },
-				typescriptreact = { "prettier" },
-				css = { "prettier" },
-				html = { "prettier" },
-				json = { "prettier" },
-				--yaml = { "prettier" },
-				markdown = { "prettier" },
-				lua = { "stylua" },
-				go = { "goimports", "gofmt" },
-				python = { "isort", "black" },
-				rust = { "rustfmt" },
-				sh = { "shfmt" },
-				bash = { "shfmt" },
-			},
-			format_on_save = {
-				lsp_fallback = "fallback",
-				async = false,
-				timeout_ms = 1000,
-			},
-		})
-	end,
+	cmd = { "ConformInfo" },
+	opts = {
+		formatters_by_ft = {
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+			javascriptreact = { "prettier" },
+			typescriptreact = { "prettier" },
+			css = { "prettier" },
+			html = { "prettier" },
+			json = { "prettier" },
+			--yaml = { "prettier" },
+			markdown = { "prettier" },
+			lua = { "stylua" },
+			go = { "goimports", "gofmt" },
+			python = { "isort", "black" },
+			rust = { "rustfmt" },
+			sh = { "shfmt" },
+			bash = { "shfmt" },
+			cs = { lsp_format = "fallback" },
+		},
+		format_on_save = {
+			lsp_format = "fallback",
+			timeout_ms = 500,
+		},
+	},
 }
